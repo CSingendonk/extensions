3280d4352b19ada8f45fcf8bb2bdb1dc500b8e14
CSingendonk <149417592+CSingendonk@users.noreply.github.com>
2025-02-08 20:55:39 -0700

    Update interception.js
    Signed-off-by: CSingendonk <149417592+CSingendonk@users.noreply.github.com>

--- interception.js
+++ interception.js
@@ -2,7 +2,7 @@
  * @version 1.3.3.7
  * @satisfies Myself ;)
  * @copyright 2024-2034 Chris Singendonk
- * @author Chris Singendonk <github.com/csingendonk>
+ * @author Chris Singendonk <github.com/CSingendonk>
  * @license none
  * @summary This repository and its original / independently authored files / code authored by Chris Singendonk and scripts is not licensed for use or distribution
  , please do not use or distribute this repository without explicit written permission from the author and the copyright holder. Any prior versions obtained through github or any other means is subject to this same license.
