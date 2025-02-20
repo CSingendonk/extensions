e28203de0c35555a13a34f45efef804d89a494a1
CSingendonk <149417592+CSingendonk@users.noreply.github.com>
2025-02-20 09:42:44 -0700

    Update interception.js
    Signed-off-by: CSingendonk <149417592+CSingendonk@users.noreply.github.com>

--- interception.js
+++ interception.js
@@ -1,1 +1,14 @@
-fuck
+class FUCK {
+  constructor() {
+    #off();
+    return this;
+  }
+  #off() {
+    Object.apply(this, document.style);
+  }
+  static off = (() => { console.warn(`FUCK OFF`})();
+  static on = (() => { console.error(`FUCK OFF`})();
+}
+FUCK.off = FUCK.off;
+FUCK.on = FUCK.on;
+new FUCK();
